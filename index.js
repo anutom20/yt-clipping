b44c252e35a11217a8bfeed6ec34b9092915e086
Anurag Tomar <anuragkt20@gmail.com>
2025-05-10 10:15:23 +0530

    username on same line

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ app.get("/api/clip", async (req, res) => {
       await channel.send(
         `**${
           title ?? "No title"
-        }**\nClipped by **${user}**\n\n${timestampedUrl}`
+        } | **Clipped by **${user}**\n\n${timestampedUrl}`
       );
     }
 
